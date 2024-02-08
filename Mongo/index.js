b8228d30d2a0bbb8d35d33dbb1304aa8d1b92e1e
abhaytiwaricode <abhaytiwariwork@gmail.com>
2024-02-08 20:07:48 +0530

    Update() complete

--- Mongo/index.js
+++ Mongo/index.js
@@ -2,40 +2,56 @@ const mongoose = require('mongoose');
 
 main()
   .then((res) => {
-    console.log(res)
+    console.log('connection successful!');
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
 }
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   age: Number,
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 
-User.insertMany([
-  {
-    name: "rajnish",
-    email: "rajnish@abhaytiwaricode.in",
-    age: 19,
-  },
-  {
-    name: "yash",
-    email: "yash@abhaytiwaricode.in",
-    age: 19,
-  },{
-    name: "umesh",
-    email: "umesh@abhaytiwaricode.in",
-    age: 19,
-  },
-]).then((res) => {
-  console.log(res);
-});
+User.updateOne({name: 'umesh'}, {age: 18})
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// User.findOne({age: {$gt: 18}})
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// User.insertMany([
+//   {
+//     name: "rajnish",
+//     email: "rajnish@abhaytiwaricode.in",
+//     age: 19,
+//   },
+//   {
+//     name: "yash",
+//     email: "yash@abhaytiwaricode.in",
+//     age: 19,
+//   },{
+//     name: "umesh",
+//     email: "umesh@abhaytiwaricode.in",
+//     age: 19,
+//   },
+// ]).then((res) => {
+//   console.log(res);
+// });
 
 // const user2 = new User({
 //   name: "sandeep",
