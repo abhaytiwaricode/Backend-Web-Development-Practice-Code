6174de66cbe73c1d5b364ccca1bbee6b1fa75944
abhaytiwaricode <abhaytiwariwork@gmail.com>
2024-02-20 16:51:05 +0530

    Error Handling

--- Project/index.js
+++ Project/index.js
@@ -4,12 +4,13 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Chat = require('./models/chat.js');
 const methodOverride = require('method-override');
+const ExpressError = require('./ExpressError.js');
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('views engine', 'ejs');
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
 
 main()
   .then((res) => {
@@ -18,81 +19,139 @@ main()
   .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+  await mongoose.connect('mongodb://127.0.0.1:27017/fakewhatsapp');
+}
+
+function asyncWrap(fn) {
+  return function (req, res, next) {
+    fn(req, res, next).catch((err) => next(err));
+  };
 }
 
 // Index Route
-app.get('/chats', async (req, res) => {
-  let chats = await Chat.find();
-  res.render('index.ejs', { chats });
-});
+app.get(
+  '/chats',
+  asyncWrap(async (req, res) => {
+    let chats = await Chat.find();
+    res.render('index.ejs', { chats });
+  })
+);
 
 // New Route
-app.get('/chats/new', async (req, res) => {
+app.get('/chats/new', (req, res) => {
+  // throw new ExpressError(404, 'Page Not Found');
   res.render('new.ejs');
 });
 
 // Create Route
-app.post('/chats', (req, res) => {
-  let { from, to, message } = req.body;
-  let newChat = new Chat({
-    from: from,
-    to: to,
-    message: message,
-    created_at: new Date(),
-  });
-  newChat
-    .save()
-    .then((res) => {
-      console.log('chat was saved');
-    })
-    .catch((err) => {
-      console.log(err);
+app.post(
+  '/chats',
+  asyncWrap(async (req, res) => {
+    let { from, to, message } = req.body;
+    let newChat = new Chat({
+      from: from,
+      to: to,
+      message: message,
+      created_at: new Date(),
     });
-  res.redirect('/chats');
-});
+    newChat
+      .save()
+      .then((res) => {
+        console.log('chat was saved');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    res.redirect('/chats');
+  })
+);
+
+// NEW - Show Route
+app.get(
+  '/chats/:id',
+  asyncWrap(async (req, res, next) => {
+    let { id } = req.params;
+    let chat = await Chat.findById(id);
+    if (!chat) {
+      next(new ExpressError(404, 'Chat Not Found'));
+    }
+    res.render('show.ejs', { chat });
+  })
+);
 
 // Edit Route
-app.get('/chats/:id/edit', async (req, res) => {
-  let { id } = req.params;
-  let chat = await Chat.findById(id);
-  res.render('edit.ejs', { chat });
-});
+app.get(
+  '/chats/:id/edit',
+  asyncWrap(async (req, res) => {
+    let { id } = req.params;
+    let chat = await Chat.findById(id);
+    res.render('edit.ejs', { chat });
+  })
+);
 
 // Update Route
-app.put('/chats/:id', async (req, res) => {
-  let { id } = req.params;
-  let { message: newMsg } = req.body;
-  let updatedChat = await Chat.findByIdAndUpdate(
-    id,
-    { message: newMsg },
-    { runValidators: true, new: true }
-  );
-  console.log(updatedChat);
-  res.redirect('/chats');
-});
+app.put(
+  '/chats/:id',
+  asyncWrap(async (req, res) => {
+    let { id } = req.params;
+    let { message: newMsg } = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(
+      id,
+      { message: newMsg },
+      { runValidators: true, new: true }
+    );
+    console.log(updatedChat);
+    res.redirect('/chats');
+  })
+);
 
 // Delete Route
-app.delete('/chats/:id', async (req, res) => {
-  let { id } = req.params;
-  res.render('confirmDelete.ejs', { id });
-});
+app.delete(
+  '/chats/:id',
+  asyncWrap(async (req, res) => {
+    let { id } = req.params;
+    res.render('confirmDelete.ejs', { id });
+  })
+);
 
 // Confirm Delete Route
-app.post('/chats/:id/delete', async (req, res) => {
-  let { id } = req.params;
-  let confirmDelete = req.body.confirmDelete;
+app.post(
+  '/chats/:id/delete',
+  asyncWrap(async (req, res) => {
+    let { id } = req.params;
+    let confirmDelete = req.body.confirmDelete;
 
-  if (confirmDelete === 'yes') {
-    let deletedChat = await Chat.findByIdAndDelete(id);
-    console.log(deletedChat);
-  }
-  res.redirect('/chats');
-});
-
+    if (confirmDelete === 'yes') {
+      let deletedChat = await Chat.findByIdAndDelete(id);
+      console.log(deletedChat);
+    }
+    res.redirect('/chats');
+  })
+);
 
 app.get('/', (req, res) => {
   res.redirect('/chats');
+});
+
+const handleValidationErr = (err) => {
+  console.log('This is a Validation error. Please follow rules');
+  console.dir(err.message);
+  return err;
+};
+
+// Mongoose Error
+app.use((err, req, res, next) => {
+  console.log(err.name);
+  if (err.name === 'ValidationError') {
+    err = handleValidationErr(err);
+  }
+  next(err);
+});
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  let { status = 500, message = 'Some Error Occurred!' } = err;
+  res.status(status).send(message);
 });
 
 app.listen(8080, () => {
--- Project/init.js
+++ Project/init.js
@@ -2,46 +2,63 @@ const mongoose = require('mongoose');
 const Chat = require('./models/chat.js');
 
 main()
-  .then((res) => {
+  .then(() => {
     console.log('connection successful!');
+    return insertChats();
   })
-  .catch((err) => console.log(err));
+  .then(() => {
+    console.log('Chats inserted successfully!');
+    return findChats();
+  })
+  .then((chats) => {
+    console.log('Found Chats:', chats);
+  })
+  .catch((err) => console.error(err))
+  .finally(() => {
+    mongoose.disconnect();
+  });
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+  await mongoose.connect('mongodb://127.0.0.1:27017/fakewhatsapp');
 }
 
-let allChats = [
-  {
-    from: 'sandeep',
-    to: 'abhay',
-    message: 'teach me JS Callbacks',
-    created_at: new Date(),
-  },
-  {
-    from: 'abhay',
-    to: 'sandeep',
-    message: 'I am sending you JS Callbacks notes',
-    created_at: new Date(),
-  },
-  {
-    from: 'yash',
-    to: 'sandeep',
-    message: 'send me your last semester exam paper',
-    created_at: new Date(),
-  },
-  {
-    from: 'umesh',
-    to: 'abhay',
-    message: 'Mera laptop aa gya',
-    created_at: new Date(),
-  },
-  {
-    from: 'yash',
-    to: 'abhay',
-    message: 'aao kaha ho!',
-    created_at: new Date(),
-  },
-];
+async function insertChats() {
+  let allChats = [
+    {
+      from: 'sandeep',
+      to: 'abhay',
+      message: 'teach me JS Callbacks',
+      created_at: new Date(),
+    },
+    {
+      from: 'abhay',
+      to: 'sandeep',
+      message: 'I am sending you JS Callbacks notes',
+      created_at: new Date(),
+    },
+    {
+      from: 'yash',
+      to: 'sandeep',
+      message: 'send me your last semester exam paper',
+      created_at: new Date(),
+    },
+    {
+      from: 'umesh',
+      to: 'abhay',
+      message: 'Mera laptop aa gya',
+      created_at: new Date(),
+    },
+    {
+      from: 'yash',
+      to: 'abhay',
+      message: 'aao kaha ho!',
+      created_at: new Date(),
+    },
+  ];
 
-Chat.insertMany(allChats);
+  await Chat.insertMany(allChats);
+}
+
+async function findChats() {
+  return Chat.find();
+}
