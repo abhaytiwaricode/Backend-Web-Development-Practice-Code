86d628f4dd08c515f98db0ea0ddf50816975cdf6
abhaytiwaricode <abhaytiwariwork@gmail.com>
2024-02-22 23:27:54 +0530

    Handling Deletion

--- Relationships/Models/customer.js
+++ Relationships/Models/customer.js
@@ -21,56 +21,94 @@ const addOrders = async () => {
       ],
     });
 
+    // customerSchema.pre('findOneAndDelete', async () => {
+    //   console.log('Pre Middleware');
+    // });
+
+    customerSchema.post('findOneAndDelete', async (customer) => {
+      try {
+        if (customer.orders.length) {
+          let res = await Order.deleteMany({
+            _id: { $in: customer.orders },
+          });
+          console.log(res);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    });
+
     const Order = mongoose.model('Order', orderSchema);
     const Customer = mongoose.model('Customer', customerSchema);
 
-    const addCustomer = async () => {
-      let cust2 = new Customer({
-        name: 'Sandeep Mishra',
+    const addCust = async () => {
+      let newCust = new Customer({
+        name: 'Yash Mishra',
       });
 
-      let order1 = await Order.findOne({ item: 'Chocolate' });
-      let order2 = await Order.findOne({ item: 'Coke' });
+      let newOrder = new Order({
+        item: 'Burger',
+        price: 199,
+      });
 
-      if (!order1 || !order2) {
-        console.log('Orders not found');
-        return;
-      }
+      newCust.orders.push(newOrder);
 
-      cust2.orders.push(order1);
-      cust2.orders.push(order2);
+      await newOrder.save();
+      await newCust.save();
 
-      let res = await cust2.save();
-      console.log(res);
+      console.log('added new customer');
     };
 
-    await addCustomer();
-
-    const findCustomer = async () => {
-      let result = await Customer.find().populate('orders');
-      console.log(result[0]);
+    const delCust = async () => {
+      let data = await Customer.findByIdAndDelete('65d6f7489ee90b67ac1f36d3');
+      console.log(data);
+      return data;
     };
 
-    await findCustomer();
+    delCust()
+      .then((deletedCustomer) => {
+        console.log(deletedCustomer);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
 
-    let res = await Order.insertMany([
-      {
-        item: 'Samosa',
-        price: 12,
-      },
-      {
-        item: 'Coke',
-        price: 20,
-      },
-      {
-        item: 'Chocolate',
-        price: 40,
-      },
-    ]);
-    console.log(res);
+    mongoose.disconnect();
+
+    // addCust
   } catch (err) {
     console.error('Connection error:', err);
   }
 };
 
 addOrders();
+
+// const addCumstomer = async () => {
+//   let cust2 = new Customer({
+//     name: 'Sandeep Mishra',
+//   });
+
+//   let order1 = await Order.findOne({ item: 'Chocolate' });
+//   let order2 = await Order.findOne({ item: 'Coke' });
+
+//   if (!order1 || !order2) {
+//     console.log('Orders not found');
+//     return;
+//   }
+
+//   cust2.orders.push(order1);
+//   cust2.orders.push(order2);
+
+//   let res = await cust2.save();
+//   console.log(res);
+
+// };
+
+// addCumstomer();
+
+// const findCustomer = async () => {
+//   let result = await Customer.find().populate('orders');
+//   console.log(result[0]);
+// };
+
+// findCustomer();
