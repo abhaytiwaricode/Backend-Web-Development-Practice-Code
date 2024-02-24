14beb29511571dae1dc5bedd5ad962adb61a04a0
abhaytiwaricode <abhaytiwariwork@gmail.com>
2024-02-24 22:55:21 +0530

    flash messags

--- Major-Project/classroom/server.js
+++ Major-Project/classroom/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const users = require('./routes/user.js');
-const posts = require('./routes/post.js');
 const session = require('express-session');
-const cookieParser = require('cookie-parser');
-app.use(cookieParser('secretcode'));
+const path = require('path');
+
+const flash = require('connect-flash');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Session middleware setup
 const sessionOptions = {
@@ -15,68 +16,30 @@ const sessionOptions = {
 };
 
 app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.successMsg = req.flash('success');
+  res.locals.errorMsg = req.flash('error');
+  next();
+});
 
 // Route to register with query parameter
 app.get('/register', (req, res) => {
   let { name = 'anonymous' } = req.query;
   req.session.name = name;
   console.log(req.session.name);
-  res.send(name);
+  if (name === 'anonymous') {
+    req.flash('error', 'user not registered!');
+  } else {
+    req.flash('success', 'user registered successfully!');
+  }
   res.redirect('/hello');
 });
 
 app.get('/hello', (req, res) => {
-  res.send(`Hello, ${req.session.name}`);
+  res.render('page.ejs', { name: req.session.name });
 });
-
-// Route to count requests using session
-app.get('/reqcount', (req, res) => {
-  if (req.session.count) {
-    req.session.count++;
-  } else {
-    req.session.count = 1;
-  }
-  res.send(`You sent a request ${req.session.count} times`);
-});
-
-// Route for testing purposes
-app.get('/test', (req, res) => {
-  res.send('test successful!');
-});
-
-// Route to set a signed cookie
-app.get('/getsignedcookie', (req, res) => {
-  res.cookie('made-in', 'India', { signed: true });
-  res.send('signed cookie sent');
-});
-
-// Route to verify signed cookies
-app.get('/varify', (req, res) => {
-  console.log(req.signedCookies);
-  res.send('verified');
-});
-
-// Route to greet based on cookie value
-app.get('/greet', (req, res) => {
-  let { name = 'anonymous' } = req.cookies;
-  res.send(`Hi, ${name}`);
-});
-
-// Route to set cookies
-app.get('/getcookies', (req, res) => {
-  res.cookie('greet', 'namaste');
-  res.send('sent you some cookies!');
-});
-
-// Root route
-app.get('/', (req, res) => {
-  console.dir(req.cookies);
-  res.send('Hi, I am root!');
-});
-
-// Using routes from external files
-app.use('/users', users);
-app.use('/posts', posts);
 
 // Server listening
 app.listen(port, () => {
