a93e2e48f5e6c188e956d1524ea6f754681f9ea4
abhaytiwaricode <abhaytiwariwork@gmail.com>
2024-02-25 13:33:54 +0530

    flash message for error

--- Major-Project/app.js
+++ Major-Project/app.js
@@ -52,6 +52,7 @@ app.use(flash());
 
 app.use((req, res, next) => {
   res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
   next();
 });
 
--- Major-Project/routes/listing.js
+++ Major-Project/routes/listing.js
@@ -37,6 +37,10 @@ router.get(
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id).populate('reviews');
+    if (!listing) {
+      req.flash('error', 'Listing you requested for does not exist!');
+      res.redirect('/listings');
+    }
     res.render('listings/show.ejs', { listing });
   })
 );
@@ -59,6 +63,10 @@ router.get(
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
+    if (!listing) {
+      req.flash('error', 'Listing you requested for does not exist!');
+      res.redirect('/listings');
+    }
     res.render('listings/edit.ejs', { listing });
   })
 );
