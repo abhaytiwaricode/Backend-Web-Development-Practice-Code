1f60cb26035aef5b165f7b865a398b41512b460c
abhaytiwaricode <abhaytiwariwork@gmail.com>
2024-03-02 12:37:39 +0530

    user logout route

--- Major-Project/routes/user.js
+++ Major-Project/routes/user.js
@@ -41,4 +41,14 @@ router.post(
   }
 );
 
+router.get('/logout', (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    req.flash('success', 'you are logged out!');
+    res.redirect('/listings');
+  });
+});
+
 module.exports = router;
