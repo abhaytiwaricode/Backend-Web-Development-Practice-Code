e3313e9efe292de2252e78ab867dab60674c3ae3
abhaytiwaricode <abhaytiwariwork@gmail.com>
2024-03-02 12:26:14 +0530

    login auth

---
+++ Major-Project/middleware.js
@@ -0,0 +1,7 @@
+module.exports.isLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    req.flash('error', 'you must be logged in to create listing!');
+    return res.redirect('/login');
+  }
+  next();
+};
--- Major-Project/routes/listing.js
+++ Major-Project/routes/listing.js
@@ -4,6 +4,7 @@ const Listing = require('../models/listing.js');
 const wrapAsync = require('../utils/wrapAsync.js');
 const ExpressError = require('../utils/ExpressError.js');
 const { listingSchema } = require('../schema.js');
+const { isLoggedIn } = require('../middleware.js');
 
 const validateListing = (req, res, next) => {
   let { error } = listingSchema.validate(req.body);
@@ -26,6 +27,7 @@ router.get(
 // New Route
 router.get(
   '/new',
+  isLoggedIn,
   wrapAsync(async (req, res) => {
     res.render('listings/new.ejs');
   })
@@ -48,6 +50,7 @@ router.get(
 //Create Route
 router.post(
   '/',
+  isLoggedIn,
   validateListing,
   wrapAsync(async (req, res) => {
     const newListing = new Listing(req.body.listing);
@@ -60,6 +63,7 @@ router.post(
 // Edit Route
 router.get(
   '/:id/edit',
+  isLoggedIn,
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
@@ -74,6 +78,7 @@ router.get(
 // Update Route
 router.put(
   '/:id',
+  isLoggedIn,
   validateListing,
   wrapAsync(async (req, res) => {
     let { id } = req.params;
@@ -86,6 +91,7 @@ router.put(
 // Delete Route
 router.delete(
   '/:id',
+  isLoggedIn,
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
