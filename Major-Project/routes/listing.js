f3f28a4393f26c771206701d7fd5f6c93cb33498
abhaytiwaricode <abhaytiwariwork@gmail.com>
2024-03-03 12:58:35 +0530

    working on file upload

--- Major-Project/routes/listing.js
+++ Major-Project/routes/listing.js
@@ -2,17 +2,21 @@ const express = require('express');
 const router = express.Router();
 const wrapAsync = require('../utils/wrapAsync');
 const listingController = require('../controllers/listings');
-
 const { isLoggedIn, isOwner, validateListing } = require('../middleware');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 router
   .route('/')
   .get(wrapAsync(listingController.index))
-  .post(
-    isLoggedIn,
-    validateListing,
-    wrapAsync(listingController.createListing)
-  );
+  // .post(
+  //   isLoggedIn,
+  //   validateListing,
+  //   wrapAsync(listingController.createListing)
+  // );
+  .post(upload.single('listing[image]'), (req, res) => {
+    res.send(req.file);
+  });
 
 // New Route
 router.get('/new', isLoggedIn, wrapAsync(listingController.renderNewForm));
